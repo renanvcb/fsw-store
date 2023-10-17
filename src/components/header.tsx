972b922934416820a0ea0e78287caf660ff1a9c9
Renan Borges <renanvcb@gmail.com>
2023-10-17 20:54:13 -0300

    feat: add header menu using sheet component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,67 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 
 export function Header() {
   return (
-    <Card className="flex justify-between p-[1.875rem] items-center">
-      <Button size="icon" variant="outline" className="p-">
-        <MenuIcon />
-      </Button>
+    <Card className="flex items-center justify-between p-[1.875rem]">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
 
-      <h1 className="font-semibold text-lg"><span className="text-primary">FSW</span> Store</h1>
+        <SheetContent side="left">
+          <SheetHeader className="items-start">
+            <SheetTitle>Menu</SheetTitle>
+          </SheetHeader>
+
+          <div className="mt-2 flex flex-col gap-2">
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <LogInIcon size={16} />
+              Fazer Login
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <HomeIcon size={16} />
+              Início
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <PercentIcon size={16} />
+              Ofertas
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <ListOrderedIcon size={16} />
+              Catálogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+
+      <h1 className="text-lg font-semibold">
+        <span className="text-primary">FSW</span> Store
+      </h1>
 
       <Button size="icon" variant="outline" className="p-">
         <ShoppingCartIcon />
-      </Button>      
+      </Button>
     </Card>
-  )
+  );
 }
