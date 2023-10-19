7992147b32c05676b57a45968e055cbc6986d740
Renan Borges <renanvcb@gmail.com>
2023-10-18 23:46:17 -0300

    feat: user redirected to home page on logo click

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -114,9 +114,11 @@ export function Header() {
         </SheetContent>
       </Sheet>
 
-      <h1 className="text-lg font-semibold">
-        <span className="text-primary">FSW</span> Store
-      </h1>
+      <Link href="/">
+        <h1 className="text-lg font-semibold">
+          <span className="text-primary">FSW</span> Store
+        </h1>
+      </Link>
 
       <Button size="icon" variant="outline" className="p-">
         <ShoppingCartIcon />
