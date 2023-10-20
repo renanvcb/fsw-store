8c0ed5305da31a4558cce740ac10c8d6038b8a5f
Renan Borges <renanvcb@gmail.com>
2023-10-20 20:35:38 -0300

    feat:  ensure cart products can be scrolled

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -5,13 +5,15 @@ import { CartContext } from "@/providers/cart";
 import CartItem from "./cart-item";
 import { computeProductPrice } from "@/helpers/products";
 import { Separator } from "./ui/separator";
+import { ScrollArea } from "./ui/scroll-area";
+import { Button } from "./ui/button";
 
 function Cart() {
   const { products, cartBasePrice, cartTotalPrice, cartTotalDiscount } =
     useContext(CartContext);
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -20,19 +22,23 @@ function Cart() {
         Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <CartItem
-              key={product.id}
-              product={computeProductPrice(product as any) as any}
-            />
-          ))
-        ) : (
-          <p className="text-center font-semibold">
-            Seu carrinho está vazio! Vamos às compras?
-          </p>
-        )}
+      <div className="flex h-full flex-col overflow-hidden">
+        <ScrollArea>
+          <div className="flex flex-col gap-5">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem
+                  key={product.id}
+                  product={computeProductPrice(product as any) as any}
+                />
+              ))
+            ) : (
+              <p className="text-center font-semibold">
+                Seu carrinho está vazio! Vamos às compras?
+              </p>
+            )}
+          </div>
+        </ScrollArea>
       </div>
 
       <div className="flex flex-col gap-3">
@@ -63,6 +69,8 @@ function Cart() {
           <p>Total</p>
           <p>R$ {cartTotalPrice.toFixed(2).replace(".", ",")}</p>
         </div>
+
+        <Button className="mt-7 font-bold uppercase">Finalizar compra</Button>
       </div>
     </div>
   );
